29b166bbd4ea7e328327d2d01ec5d75625cf8e84
Maciek Zdanowicz <m.zdanowicz@gmail.com>
2020-05-31 17:54:11 +0200

    Added sentry support (even tested:)!

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import subprocess
 import argparse
 import sys
+import sentry_sdk
 import nbformat 
 from datetime import datetime
 from datetime import date
@@ -8,34 +9,25 @@ from nbconvert.preprocessors import ExecutePreprocessor
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--force", action="store_true")
+parser.add_argument("--sentry-url")
 args = parser.parse_args()
 flagForce = args.force
+sentryUrl = args.sentry_url
 
-commandLatest = """git log --author="cronjob" -1 --date=short --pretty=format:%cd"""
-cp = subprocess.run(commandLatest, shell=True, check=True, text=True, capture_output=True)
-dateOfCommit = date.fromisoformat(cp.stdout)
-if not flagForce:
-	print("Last data push on", dateOfCommit)
-today = date.today()
-if dateOfCommit == today and not flagForce:
-	print("Already pushed today!")
-	quit()
+# sentry init
+sentry_sdk.init(dsn=sentryUrl)
 
+# Execution function
 run_path = "./"
 data_path = "Rt Data Preparation.ipynb"
 model_path = "Rainier.ipynb"
 
-print("Execution on", today)
-if flagForce:
-	print("Forcing!")
-
-# Execution function
 def execute(path,kernel):
 	with open(f"{run_path}{path}") as f:
 		nb = nbformat.read(f, as_version=4)
 		ep = ExecutePreprocessor(timeout=1800, kernel_name=kernel)
 		try:
-                	out = ep.preprocess(nb, {'metadata': {'path': run_path}})
+			out = ep.preprocess(nb, {'metadata': {'path': run_path}})
 		except CellExecutionError:
 			msg = 'Error executing the notebook "%s".\n\n' % path
 			msg += 'See notebook "%s" for the traceback.' %  f"executed{path}"
@@ -44,15 +36,34 @@ def execute(path,kernel):
 		finally:
 			with open(f"{run_path}executed_{path}", 'w', encoding='utf-8') as f:
 				nbformat.write(nb, f)
+			
 
-now = datetime.now()
+try:
+	commandLatest = """git log --author="cronjob" -1 --date=short --pretty=format:%cd"""
+	cp = subprocess.run(commandLatest, shell=True, check=True, text=True, capture_output=True)
+	dateOfCommit = date.fromisoformat(cp.stdout)
+	if not flagForce:
+		print("Last data push on", dateOfCommit)
+	today = date.today()
+	if dateOfCommit == today and not flagForce:
+		print("Already pushed today!")
+		quit()
 
-# Running data preparation and model notebooks
-execute(data_path,'python3')
-execute(model_path,'rainier')
+	print("Execution on", today)
+	if flagForce:
+		print("Forcing!")
 
-# Pushing everything to git repository
-commandConfig = """git config user.name "cronjob" && git config user.email hello@rteu.live"""
-commandExecution = f"""git commit -a -m"Updating rt-rainier.csv on {now}" && git push"""
-subprocess.run(commandConfig, shell=True, check=True)
-subprocess.run(commandExecution, shell=True, check=True)
+	now = datetime.now()
+
+	# Running data preparation and model notebooks
+	execute(data_path,'python3')
+	execute(model_path,'rainier')
+
+	# Pushing everything to git repository
+	commandConfig = """git config user.name "cronjob" && git config user.email hello@rteu.live"""
+	commandExecution = f"""git commit -a -m"Updating rt-rainier.csv on {now}" && git push"""
+	subprocess.run(commandConfig, shell=True, check=True)
+	subprocess.run(commandExecution, shell=True, check=True)
+except Exception as e:
+	sentry_sdk.capture_exception(e)
+	raise e
