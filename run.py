cc176ced99d006ccb8fbb7b5a95ea78e6df1bcf6
cronjob <hello@rteu.live>
2020-06-04 08:31:22 +0000

    Updating rt-rainier.csv on 2020-06-04 06:40:45.279953

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ model_path = "Rainier.ipynb"
 def execute(path,kernel):
 	with open(f"{run_path}{path}") as f:
 		nb = nbformat.read(f, as_version=4)
-		ep = ExecutePreprocessor(timeout=3600, kernel_name=kernel)
+		ep = ExecutePreprocessor(timeout=14400, kernel_name=kernel)
 		try:
 			out = ep.preprocess(nb, {'metadata': {'path': run_path}})
 		except CellExecutionError:
